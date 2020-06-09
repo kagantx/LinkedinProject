d9e0fd0b2469708ee642e483e60edfd13d91bf77
kagantx <kagantx@gmail.com>
2020-06-09 16:21:37 +0300

    adding pagescrape.py

--- pagescrape.py
+++ pagescrape.py
@@ -3,35 +3,41 @@ import requests
 from bs4 import BeautifulSoup
 from collections import Counter
 class WebPage:
+
     def __init__(self,url,field_dictionary):
         self.url=url
-        self.field_dictionary=field_list
+        self.field_dictionary=field_dictionary
+        self.data_dictionary={}
+        self.
     def get_data(self):
-        """field_list is a list of lists with each top list being a category"""
-        page=requests.get(url)
+        """Obtain a dictionary containing the text for each field name,as well as a
+        counter object for each text"""
+        page=requests.get(self.url)
         counter_list=[]
         counter_list.append()
-        soup=BeautifulSoup(page.text,'html.parser')
+        soup=BeautifulSoup(page.text)
 
         print(soup.prettify())
-        data_dictionary=[]
-        for tag,text in field_list:
-            field_text=
-            data_dictionary.append(field_name:(field_text,get_wordcount_dictionary(field_text))
-        return data_dictionary
+        self.data_dictionary= {}
+        for field_name,field_content in self.field_dictionary.items():
+            self.add_webitem(field_name,field_content)
 
-    def get_wordcount_dictionary(self):
+    def add_webitem(self,field_name,field_content):
+        s
+        field_name: (field_name, self.get_wordcount_dictionary(field_name))}
+
+    def get_wordcount_dictionary(self,text):
         """Obtains word list from file text, after filtering out punctuation"""
-        file_fixed = re.sub(r'[!@:#$_.*;&"`,\]\[?\-()]', ' ', file_text)
-        # Remove pronunciation marks that are never part of a word
+        file_fixed = re.sub(r'[!@:#$_.*;&"`,\]\[?\-()]', ' ', text)
+        # Remove punctuation marks that are never part of a word
         file_fixed_two = re.sub(r'\s\'', ' ', file_fixed)
         file_fixed_three = re.sub(r'\'\s', ' ', file_fixed_two)
         # Remove leading and trailing apostrophes, but not those inside words
         return Counter(file_fixed_three.lower().split())
         # Return a counter object of the words, with all words in lowercase
 if __name__=='__main__':
-    url_test="https://www.linkedin.com/jobs/view/1876270330/?alternateChannel=search
-    WebPage()
+    url_test="https://www.linkedin.com/in/daniel-kagan-691b7b182/"
+    WebPage(url_test,{'job_titles':[]})
     print("All tests passed")
 
 

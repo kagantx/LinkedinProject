a2264e723e6df0314f3e08739c864e343dd693ae
kagantx <kagantx@gmail.com>
2020-06-17 02:20:47 +0300

    Adding Docstrings

--- scrapewebpage.py
+++ scrapewebpage.py
@@ -3,22 +3,6 @@ from time import sleep
 import json
 import pprint
 
-"""my_dict={featured:{num_featured}
-experience:{num_jobs:,
-            jobs: [{company_name:, position:position,start_date:
-                    end_date:}
-                   ...]
-
-education:{num_education:x,
-            education: [institution_name: {degree_name:degree,start_date:                   }
-                   ...]
-
-skills_templagte: {num_skills:x,
-        skills:
-        {skill_name:n_endorsements,
-         }"""
-
-
 class WebPage:
     """Represents a LinkedIn webpage.
 
@@ -26,25 +10,39 @@ class WebPage:
      __init__ to set variables and validate the user's choice of sections to scrape
      get_data to scrape the chosen sections
      _parse_(section) functions to parse each section
-     print_data() to print the data
-     to scrape a LinkedIn profile page and
+     print_data to print the data
+     dump_json to dump the data to a json file
+
+    Contains class constants:
+    DEFAULT_SECTIONS (default sections to scrape)
+    ALL_SECTIONS (all sections possible to scrape)
+
+    (section)_FIELDS (fields to scrape for each section
+    SCROLL_PAUSE_TIME (time to wait while scrolling to input variables
+
+    Contains instance variables:
+    url_loc - web page location
+    scraped_data - data scraped from web page
 
 
     """
-    DEFAULT_ITEMS = {"Experience", "Education"}
+    DEFAULT_SECTIONS = {"Experience", "Education"}
 
-    ALL_ITEMS = {"Experience", "Education", "Skills", "Featured"}
+    ALL_SECTIONS = {"Experience", "Education", "Skills", "Featured"}
     EXPERIENCE_FIELDS = {'Company Name', 'Dates Employed', 'Employment Duration', 'Location'}
     EDUCATION_FIELDS = {'Degree Name', 'Field Of Study', "Dates attended or expected graduation"}
     FIELDS = EXPERIENCE_FIELDS.union(EDUCATION_FIELDS)
+    SCROLL_PAUSE_TIME = 1
 
     def __init__(self, url_loc, my_driver, section_list=None):
-        """Initializes the WebPage class. Accepts a ur"""
+        """Initializes the WebPage class. Accepts a url for the page and an optional section list.
+         Also initializes the Selenium driver for scraping the webpage and the scraped_data
+         variable that contains the results"""
         self.url_loc = url_loc
         if section_list is None:
-            section_list = self.DEFAULT_ITEMS
+            section_list = self.DEFAULT_SECTIONS
         self.section_list = [section_item.title() for section_item in section_list]
-        if not self.ALL_ITEMS >= set(self.section_list):
+        if not self.ALL_SECTIONS >= set(self.section_list):
             raise ValueError("You asked for a LinkedIn section that we cannot scrape.")
         self.scraped_data = {}
         self.driver = my_driver
@@ -52,7 +50,7 @@ class WebPage:
 
     def get_data(self):
         """First, scrolls through the page to make sure it is loaded
-        Then, gets data for all sections unless excluded"""
+        Then, scrapes data for all sections in self.section_list"""
         self.driver.get(self.url_loc)
         self._scroll_page()
 
@@ -69,21 +67,23 @@ class WebPage:
         if "Featured" in self.section_list:
             featured = self.driver.find_elements_by_xpath
             self.scraped_data["Featured"] = self._parse_featured(featured)
+        self.driver.close()
 
     def _scroll_page(self):
-        SCROLL_PAUSE_TIME = 1
+
         self.driver.execute_script("document.body.style.zoom='10%'")
         self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight/2));")
         sleep(SCROLL_PAUSE_TIME)
         self.driver.execute_script("window.scrollTo(0, (document.body.scrollHeight));")
 
     def _parse_experience(self, experience):
-
+        """Parses the experience section using the raw scraped data. Records the number of jobs
+        and then produces an array containing the EXPERIENCE_FIELDS for each job"""
         experience_dict = {}
         experience_dict.update({"Number": len(experience)})
         experience_list = []
-        for item in experience:
-            job_fields = item.text.split('\n')
+        for job in experience:
+            job_fields = job.text.split('\n')
             print(job_fields)
             experience_list.append(self.create_dictionary_from_pairs(job_fields))
 
@@ -91,30 +91,42 @@ class WebPage:
         return experience_dict
 
     def _parse_education(self, education):
+        """Parses the education section using the raw scraped data. Records the number of institutions
+         attended and then produces an array containing the EDUCATION_FIELDS and their values for each job"""
         education_dict = {}
         education_dict.update({"Number": len(education)})
 
         education_list = []
-        for item in education:
-            degree_fields = item.text.split('\n')
+        for institution in education:
+            degree_fields = institution.text.split('\n')
             education_list.append(self.create_dictionary_from_pairs(degree_fields))
         education_dict.update({"University_List": education_list})
         return education_dict
 
     def _parse_featured(featured, self):
+        """Parses the featured section using the raw scraped data.
+        Returns the number of items in the section as a key:value pair"""
+
         pass
 
     def _parse_skills(skills, self):
+        """Parses the skills section using the raw scraped data.
+        Records the number of skills and dictionary of the form  skill: number of endorsements"""
         pass
 
     def export_json(self, out_name):
+        """Exports the scraped data from the web page as a json file"""
         with open(out_name + '.json', 'w') as json_dump:
             json.dump(json_dump, self.scraped_data)
 
     def print_data(self):
+        """pprints the scraped data from the web page"""
         pprint.pprint(self.scraped_data)
 
     def create_dictionary_from_pairs(self, item_list):
+        """Given a list, creates a dictionary with key equal to the first item in the list
+        and value equal to a dictionary containing keys for entries present in self.FIELDS
+        and values equal to the following item in item_list"""
         print(item_list)
         sub_dict = {item_list[x]: item_list[x + 1] for x in range(1, len(item_list) - 1)
                     if item_list[x] in self.FIELDS and len(item_list[x]) > 0}

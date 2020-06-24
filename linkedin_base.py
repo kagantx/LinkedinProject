9ec3bd0f754b0fa8667a3421f7fdbd8fce9470be
kagantx <kagantx@gmail.com>
2020-06-24 10:14:31 +0300

    Edit documentation

--- linkedin_base.py
+++ linkedin_base.py
@@ -6,7 +6,7 @@ from scrape_page import WebPage
 import pickle
 
 
-class LikenBot():
+class LinkedinBot:
     """Represents a LinkedIn url profile scraper bot.
 
         Contains methods:
@@ -17,14 +17,14 @@ class LikenBot():
             load_result function to load the scrape data to a pickle file
 
         Contains class constants:
-            PREFIX = "https://www.linkedin.com/in/"
+            PREFIX ="https://www.linkedin.com/in/"
      """
 
     PREFIX = "https://www.linkedin.com/in/"
 
     def __init__(self, job="data scientist", location="Tel-Aviv", nb_page=2):
 
-        """ Initializes the LikenBot class. Also initializes the Selenium driver for scraping the webpage
+        """ Initializes the LinkedinBot class. Also initializes the Selenium driver for scraping the webpage
             and the dictionaries that contains the results
 
             Parameters:
@@ -36,22 +36,22 @@ class LikenBot():
         self.driver = webdriver.Chrome()
         # initialize data
         self.url_dic = {}
-        self.final_dic_result = {}
+        self.scraped_page_data = {}
         self.job = job
         self.location = location
         self.nb_page = nb_page
 
     def login(self):
-        """ The function login inside the Linkedin web site"""
+        """ This function logs in to the LinkedIn web site"""
         self.driver.get('https://www.linkedin.com/')
 
-        # identification button
+        # Ask LinkedIn to let us log in
         login_btn_1 = self.driver.find_element_by_xpath('/html/body/nav/a[3]')
         login_btn_1.click()
 
         sleep(1)
 
-        # enter the mail
+        # Enter email address as username
         email_in = self.driver.find_element_by_xpath('//*[@id="username"]')
         email_in.send_keys(MAIL)
 
@@ -59,56 +59,60 @@ class LikenBot():
         pw_in = self.driver.find_element_by_xpath('//*[@id="password"]')
         pw_in.send_keys(PASSWORD)
 
-        # connect button
+        # log in to the website
         login_btn_2 = self.driver.find_element_by_xpath('//*[@id="app__container"]/main/div[2]/form/div[3]/button')
         login_btn_2.click()
 
-    def scrap_url_profile(self):
-        """ The function will scrape linkedin url profile on google"""
+    def scrape_url_profiles(self):
+        """ This function searches google for profiles on LinkedIn
+        that match the search terms given by the user"""
         self.driver.get('https://www.google.com/')
 
         # the search bar to input query
         query = self.driver.find_element_by_xpath('//*[@id="tsf"]/div[2]/div[1]/div[1]/div/div[2]/input')
 
-        # the query we put in the google browser
+        # Seach google for our search terms in the google browser
         query.send_keys(f'site:linkedin.com/in/ AND "{self.job}" AND "{self.location}"')
         query.send_keys(Keys.RETURN)
         sleep(1)
 
         for i in range(self.nb_page):
 
-            # contain second part of the url of all the profile of the page in list
+            # Find links that correspond to search results
             linkedin_urls = self.driver.find_elements_by_class_name('eipWBe')
 
-            # create our valid url without '...' and with length > 0 and save it in url_dic
+            # Find valid urls by ensuring they do not contain '...' and have length > 0
+            # If they are not already in the url_dic, save them in url_dic
             for url in linkedin_urls:
                 if len(url.text) != 0 and url.text[-3:] != '...':
                     if self.PREFIX + url.text[2:] + '/' not in self.url_dic:
                         self.url_dic[self.PREFIX + url.text[2:] + '/'] = 1
 
-            # Try to click on google next page to scrap new profile until possible
+            # Try to click on next google page to scrape new profile until it becomes impossible
             try:
                 next_btn = self.driver.find_element_by_xpath('//*[@id="pnnext"]/span[2]')
                 next_btn.click()
                 sleep(2)
 
             except:
-                print('No more clickable')
+                print('Unable to search for any more profiles')
                 break
 
-        # the list of all the linkedin scrape profile url
-        list_of_url = self.url_dic.keys()
+        # Get the list of all the LinkedIn profiles
+        list_of_urls = self.url_dic.keys()
 
-        # Loop on the url linkedin profile, to scrape data on it using the class WebPage from scrape_page.py
-        for url_profile in list_of_url:
+        # Loop over all urls to scrape data on it using the class WebPage from scrape_page.py
+        for url_profile in list_of_urls:
             try:
                 sleep(1)
-                my_class = WebPage(url_profile, self.driver)
-                my_class.get_data()
-                self.final_dic_result.update(my_class.export_data())
+                this_url = WebPage(url_profile, self.driver)
+                this_url.get_data()
+                self.scraped_page_data.update(this_url.export_data())
 
 
-            except:
+            except Exception as ex:
+                print("Got Exception {} while trying to scrape profile {}".format(ex,url_profile))
+                print("Moving on to next profile")
                 continue
 
         self.driver.close()
@@ -117,11 +121,11 @@ class LikenBot():
         """The function saves our result in a pickle file"""
 
         dbfile = open('profile_pickle', 'wb')
-        pickle.dump(self.final_dic_result, dbfile)
+        pickle.dump(self.scraped_page_data, dbfile)
         dbfile.close()
 
     def load_result(self):
-        """The function load our result from the pickle file"""
+        """The function loads our result from the pickle file"""
 
         dbfile = open('profile_pickle', 'rb')
         db = pickle.load(dbfile)
@@ -130,8 +134,8 @@ class LikenBot():
 
 
 if __name__ == '__main__':
-    bot = LikenBot()
+    bot = LinkedinBot()
     bot.login()
-    bot.scrap_url_profile()
+    bot.scrape_url_profiles()
     bot.save_result()
     bot.load_result()
